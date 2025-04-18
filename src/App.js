a0c8a43bfeb310faa8f717d7dd0ede85014d1271
hardik-pithadia <hardikrpithadia@gmail.com>
2025-04-18 16:59:39 +0530

    data operations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,202 @@
-export default function App() {
-	return (
-		<div>
-			<h1>Welcome to my First React App!</h1>
-		</div>
-	);
+// export default function App() {
+// 	return (
+// 		<div>
+// 			<h1>Welcome to my First React App!</h1>
+// 		</div>
+// 	);
+// }
+
+/* eslint-disable no-unused-expressions */
+const data = [
+	{
+		id: 1,
+		title: "The Lord of the Rings",
+		publicationDate: "1954-07-29",
+		author: "J. R. R. Tolkien",
+		genres: [
+			"fantasy",
+			"high-fantasy",
+			"adventure",
+			"fiction",
+			"novels",
+			"literature",
+		],
+		hasMovieAdaptation: true,
+		pages: 1216,
+		translations: {
+			spanish: "El señor de los anillos",
+			chinese: "魔戒",
+			french: "Le Seigneur des anneaux",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.52,
+				ratingsCount: 630994,
+				reviewsCount: 13417,
+			},
+			librarything: {
+				rating: 4.53,
+				ratingsCount: 47166,
+				reviewsCount: 452,
+			},
+		},
+	},
+	{
+		id: 2,
+		title: "The Cyberiad",
+		publicationDate: "1965-01-01",
+		author: "Stanislaw Lem",
+		genres: [
+			"science fiction",
+			"humor",
+			"speculative fiction",
+			"short stories",
+			"fantasy",
+		],
+		hasMovieAdaptation: false,
+		pages: 295,
+		translations: {},
+		reviews: {
+			goodreads: {
+				rating: 4.16,
+				ratingsCount: 11663,
+				reviewsCount: 812,
+			},
+			librarything: {
+				rating: 4.13,
+				ratingsCount: 2434,
+				reviewsCount: 0,
+			},
+		},
+	},
+	{
+		id: 3,
+		title: "Dune",
+		publicationDate: "1965-01-01",
+		author: "Frank Herbert",
+		genres: ["science fiction", "novel", "adventure"],
+		hasMovieAdaptation: true,
+		pages: 658,
+		translations: {
+			spanish: "",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.25,
+				ratingsCount: 1142893,
+				reviewsCount: 49701,
+			},
+		},
+	},
+	{
+		id: 4,
+		title: "Harry Potter and the Philosopher's Stone",
+		publicationDate: "1997-06-26",
+		author: "J. K. Rowling",
+		genres: ["fantasy", "adventure"],
+		hasMovieAdaptation: true,
+		pages: 223,
+		translations: {
+			spanish: "Harry Potter y la piedra filosofal",
+			korean: "해리 포터와 마법사의 돌",
+			bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+			portuguese: "Harry Potter e a Pedra Filosofal",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.47,
+				ratingsCount: 8910059,
+				reviewsCount: 140625,
+			},
+			librarything: {
+				rating: 4.29,
+				ratingsCount: 120941,
+				reviewsCount: 1960,
+			},
+		},
+	},
+	{
+		id: 5,
+		title: "A Game of Thrones",
+		publicationDate: "1996-08-01",
+		author: "George R. R. Martin",
+		genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+		hasMovieAdaptation: true,
+		pages: 835,
+		translations: {
+			korean: "왕좌의 게임",
+			polish: "Gra o tron",
+			portuguese: "A Guerra dos Tronos",
+			spanish: "Juego de tronos",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.44,
+				ratingsCount: 2295233,
+				reviewsCount: 59058,
+			},
+			librarything: {
+				rating: 4.36,
+				ratingsCount: 38358,
+				reviewsCount: 1095,
+			},
+		},
+	},
+];
+
+function getBooks() {
+	return data;
 }
+
+function getBook(id) {
+	return data.find((d) => d.id === id);
+}
+
+const books = getBooks();
+books;
+
+const book = getBook(3);
+
+const { title, author, publicationDate, genres } = book;
+console.log("Title : ", title);
+console.log("Author : ", author);
+console.log("Publication Date : ", publicationDate);
+console.log("Generes : ", genres);
+
+const { ...otherGeneres } = genres;
+console.log("Other Generes : ", otherGeneres);
+
+const newGeneres = [...genres, "new data"];
+console.log("newGeneres : ", newGeneres);
+
+const summary = `${title} is nice book`;
+summary;
+
+function getYear(str) {
+	return str.split("-")[0];
+}
+
+console.log(getYear(publicationDate));
+
+const totalVal = [1, 2, 3, 4, 5].map((val) => val * 2);
+totalVal;
+
+const bookPages = books.filter((book) => book.pages > 1000);
+bookPages;
+
+const arrayObj = [1, 5, 6, 3, 2, 4];
+const sortedArray = arrayObj.sort((a, b) => a - b);
+sortedArray;
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+// 	.then((res) => res.json())
+// 	.then((response) => {
+// 		console.log("Json Response : ", response);
+// 	});
+
+async function getTools() {
+	const responseObj = await fetch("https://jsonplaceholder.typicode.com/todos");
+	const jsonObj = await responseObj.json();
+	console.log(jsonObj);
+}
+getTools();
